9e67fa85f37d2a54b6981b23bc2cbfcd50729a45
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-02-11 12:47:35 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: XXX
- * Created on: Sep 2020
+ * Created on: Feb 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Devon!')
